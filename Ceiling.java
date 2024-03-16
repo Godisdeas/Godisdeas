e3870aa6a77ab83c91742b0479dfe69a96c3acfe
Godisdeas <146644943+Godisdeas@users.noreply.github.com>
2024-03-16 20:38:20 +0530

    Update Ceiling.java

--- Ceiling.java
+++ Ceiling.java
@@ -4,6 +4,9 @@ public class Ceiling {
 
         int s = 0;
         int e = arr.length - 1;
+        if(target > arr[e]){
+            System.out.print("Ceiling not Found"); }
+        else{
         while(s<=e){
             int mid = s + (e-s)/2;
 
@@ -19,6 +22,7 @@ public class Ceiling {
 
         }
         return s;
+        }
 
     }
     public static void main(String[] args) {
